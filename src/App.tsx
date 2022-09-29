06036a0251742bf35f029ccfcb16dd00b2342df7
James <jamestariga08@live.com>
2022-09-28 20:12:23 -0700

    🔥 Implemented React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+import Layout from './Components/Layout'
+import Home from './Page/Home'
+import About from './Page/About'
+
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Route>
+    )
+  )
+
   return (
-    <div>
-      <h1 className='text-3xl font-bold'>Hello world!</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
