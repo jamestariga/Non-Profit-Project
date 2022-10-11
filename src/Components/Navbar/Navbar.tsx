09cac02f733e43530c84c6266e7b789696222141
James <jamestariga08@live.com>
2022-10-11 16:38:44 -0700

    🔥 Updated the responsive menu

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
 import useEventListener from '../../hooks/useEventListener'
+import { HiOutlineMenuAlt1 } from 'react-icons/hi'
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState<boolean>(false)
@@ -25,52 +26,44 @@ const Navbar = () => {
         <div className='navbar-start'>
           <div className='dropdown'>
             <label tabIndex={0} className='btn btn-ghost lg:hidden'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h8m-8 6h16'
-                />
-              </svg>
+              <HiOutlineMenuAlt1 className='h-8 w-8 text-red-500' />
             </label>
             <ul
               tabIndex={0}
               className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
             >
               <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className='justify-between'>
-                  Parent
-                  <svg
-                    className='fill-current'
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='24'
-                    height='24'
-                    viewBox='0 0 24 24'
-                  >
-                    <path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' />
-                  </svg>
-                </a>
-                <ul className='p-2'>
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <NavLink
+                  to='/'
+                  className={({ isActive }) =>
+                    isActive ? `bg-red-500 text-white` : `text-gray-200`
+                  }
+                  end
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
-                <a>Item 3</a>
+                <NavLink
+                  to='/about'
+                  className={({ isActive }) =>
+                    isActive ? `bg-red-500 text-white` : `text-gray-200`
+                  }
+                  end
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to='/faq'
+                  className={({ isActive }) =>
+                    isActive ? `bg-red-500 text-white` : `text-gray-200`
+                  }
+                  end
+                >
+                  FAQ
+                </NavLink>
               </li>
             </ul>
           </div>
