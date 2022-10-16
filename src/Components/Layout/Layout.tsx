e1e7dabb9b18d32b5fd5395e5a75a35598ba27fc
James <jamestariga08@live.com>
2022-10-16 11:40:03 -0700

    🔥 When page transition occurs, it now starts at the top

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,12 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
 import Navbar from '../Navbar'
+
+const Wrapper = ({ children }: any) => {
+  const location = useLocation()
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0)
+  }, [location.pathname])
+
+  return children
+}
 
 const Layout = () => {
   return (
-    <main>
+    <Wrapper>
       <Navbar />
       <Outlet />
-    </main>
+    </Wrapper>
   )
 }
 
