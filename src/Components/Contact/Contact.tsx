f4396ab9c4fadc1091f021e7270f2507ef807f95
jamestariga <jamestariga08@live.com>
2022-10-18 19:56:05 -0700

    🔥 Minor update

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <section className='bg-white dark:bg-gray-900'>
+    <section className='light:bg-white bg-gray-900'>
       <div className='py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
         <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white'>
           Contact Us
